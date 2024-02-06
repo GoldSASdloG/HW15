0109d7b13bbc303abf37e5c23e6a1e9a3157f2b5
GoldSASdloG <Procatulus@gmail.com>
2024-02-06 15:24:48 +0300

    home work is ready! home work num2 is ready!

--- Task2/src/Main.java
+++ Task2/src/Main.java
@@ -1,5 +1,33 @@
+import java.util.Scanner;
+
 public class Main {
+
+    public static final String CORRECT_ALFABET = "абвгдеёжзийклмнопрстуфхцчшщъыьэюяАБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ -";
+
     public static void main(String[] args) {
         //todo код писать тут. По желанию(рекомендуется) вынести код в методы.
+
+        boolean isValid = true;
+        int spaseCount = 0;
+        System.out.println("Введите ФИО:");
+        String inputFio = new Scanner(System.in).nextLine().trim();
+
+        for (int i = 0; i < inputFio.length(); i++) {
+            if (!CORRECT_ALFABET.contains(String.valueOf(inputFio.charAt(i)))){
+//                System.out.println("Строка не валидна!!!");
+                isValid = false;
+                break;
+            }
+            if (inputFio.charAt(i) == ' '){
+                spaseCount++;
+            }
+        }
+        if (isValid && spaseCount == 2){
+            System.out.println("Фамилия:  " + inputFio.substring(0, inputFio.indexOf(" ")).trim());
+            System.out.println("Имя:  " + inputFio.substring(inputFio.indexOf(" "), inputFio.lastIndexOf(" ")).trim());
+            System.out.println("Отчество:  " + inputFio.substring(inputFio.lastIndexOf(" ")).trim());
+        } else {
+            System.out.println("Введенная строка не является ФИО");
+        }
     }
 }
